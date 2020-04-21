b7dc5533eab48c41093ce740aa84304e873f56dc
unknown <kevinredahan@gmail.com>
2020-04-21 11:59:34 +0100

    Generating Circle of Traffic Lights

--- Assets/Scripts/SpawnTrafficLights.cs
+++ Assets/Scripts/SpawnTrafficLights.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class SpawnTrafficLights : MonoBehaviour
 {
+    public int NoOfTrafficLights = 10;
+    public GameObject[] TrafficLights; // An array of GameObjects that holds each traffic light
+    public Vector3 carStartPoint;
+    GameObject Car;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Car = GameObject.FindGameObjectWithTag("Car");
+        carStartPoint = Car.transform.position;
+        //carStartPoint = FindObjectOfType
+        instantiateInCircle(TrafficLights,carStartPoint,NoOfTrafficLights); // Passing in the Traffic Light array, the start position and the number of lights
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void instantiateInCircle(GameObject[] obj, Vector3 location, int NoLights)
+    {
+        for (int i = 0; i < NoLights; i++)
+        {
+            float radius = NoLights; // ie: Radius == 10
+            float angle = i * Mathf.PI * 2f / radius; // ex: 1 * 3.14 * 2f / 10
+            //Vector3 newPos = transform.position + (new Vector3(Mathf.Cos(angle) * radius, -2, Mathf.Sin(angle) * radius));
+            Vector3 newPos = location + (new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius));
+            Instantiate(obj[i], newPos, Quaternion.Euler(0, 0, 0));
+        }
     }
 }
